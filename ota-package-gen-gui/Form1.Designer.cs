890e52013d1e04793d6e8968730a77bead5c6f8e
quandosale <fang.chp@gmail.com>
2017-06-02 23:32:38 +0800

    fix save name

--- ota-package-gen-gui/Form1.Designer.cs
+++ ota-package-gen-gui/Form1.Designer.cs
@@ -28,69 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.keyfile_url = new System.Windows.Forms.TextBox();
-            this.btn_keyfile = new System.Windows.Forms.Button();
             this.btn_package = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btn_keyfile_browse = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.firmware_url = new System.Windows.Forms.TextBox();
             this.btn_firmware_browse = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // keyfile_url
-            // 
-            this.keyfile_url.Enabled = false;
-            this.keyfile_url.Location = new System.Drawing.Point(65, 20);
-            this.keyfile_url.Margin = new System.Windows.Forms.Padding(0);
-            this.keyfile_url.Name = "keyfile_url";
-            this.keyfile_url.Size = new System.Drawing.Size(274, 20);
-            this.keyfile_url.TabIndex = 0;
-            // 
-            // btn_keyfile
-            // 
-            this.btn_keyfile.Location = new System.Drawing.Point(183, 83);
-            this.btn_keyfile.Name = "btn_keyfile";
-            this.btn_keyfile.Size = new System.Drawing.Size(75, 23);
-            this.btn_keyfile.TabIndex = 1;
-            this.btn_keyfile.Text = "Keyfile";
-            this.btn_keyfile.UseVisualStyleBackColor = true;
-            this.btn_keyfile.Click += new System.EventHandler(this.btn_keyfile_Click);
-            // 
             // btn_package
             // 
-            this.btn_package.Location = new System.Drawing.Point(264, 83);
+            this.btn_package.Location = new System.Drawing.Point(301, 52);
             this.btn_package.Name = "btn_package";
             this.btn_package.Size = new System.Drawing.Size(75, 23);
             this.btn_package.TabIndex = 2;
-            this.btn_package.Text = "Package";
+            this.btn_package.Text = "Generate";
             this.btn_package.UseVisualStyleBackColor = true;
             this.btn_package.Click += new System.EventHandler(this.btn_package_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Keyfile:";
-            // 
-            // btn_keyfile_browse
-            // 
-            this.btn_keyfile_browse.Location = new System.Drawing.Point(348, 19);
-            this.btn_keyfile_browse.Margin = new System.Windows.Forms.Padding(0);
-            this.btn_keyfile_browse.Name = "btn_keyfile_browse";
-            this.btn_keyfile_browse.Size = new System.Drawing.Size(24, 22);
-            this.btn_keyfile_browse.TabIndex = 4;
-            this.btn_keyfile_browse.Text = "···";
-            this.btn_keyfile_browse.UseVisualStyleBackColor = true;
-            this.btn_keyfile_browse.Click += new System.EventHandler(this.btn_keyfile_browse_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 53);
+            this.label2.Location = new System.Drawing.Point(14, 21);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 6;
@@ -99,7 +56,7 @@
             // firmware_url
             // 
             this.firmware_url.Enabled = false;
-            this.firmware_url.Location = new System.Drawing.Point(65, 50);
+            this.firmware_url.Location = new System.Drawing.Point(69, 18);
             this.firmware_url.Margin = new System.Windows.Forms.Padding(0);
             this.firmware_url.Name = "firmware_url";
             this.firmware_url.Size = new System.Drawing.Size(274, 20);
@@ -107,7 +64,7 @@
             // 
             // btn_firmware_browse
             // 
-            this.btn_firmware_browse.Location = new System.Drawing.Point(348, 50);
+            this.btn_firmware_browse.Location = new System.Drawing.Point(352, 17);
             this.btn_firmware_browse.Margin = new System.Windows.Forms.Padding(0);
             this.btn_firmware_browse.Name = "btn_firmware_browse";
             this.btn_firmware_browse.Size = new System.Drawing.Size(24, 22);
@@ -120,15 +77,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(387, 125);
+            this.ClientSize = new System.Drawing.Size(387, 89);
             this.Controls.Add(this.btn_firmware_browse);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.firmware_url);
-            this.Controls.Add(this.btn_keyfile_browse);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_package);
-            this.Controls.Add(this.btn_keyfile);
-            this.Controls.Add(this.keyfile_url);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DFU Package Generator";
@@ -138,12 +91,7 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox keyfile_url;
-        private System.Windows.Forms.Button btn_keyfile;
         private System.Windows.Forms.Button btn_package;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btn_keyfile_browse;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox firmware_url;
         private System.Windows.Forms.Button btn_firmware_browse;
--- ota-package-gen-gui/Form1.cs
+++ ota-package-gen-gui/Form1.cs
@@ -14,8 +14,9 @@ namespace ota_package_gen_gui
 {
     public partial class Form1 : Form
     {
-        String filepath = "E:\\Work\\On\\sportsapp\\nrf\\ota-package-gen\\dist\\__main__.exe";
+        String filepath = "dist\\__main__.exe";
         String param_keyfile = "keys generate temp.pem";
+        String firmware_filename = "";
         String param_package = "pkg generate --hw-version 52 --sd-req 0x98,0xCAFE --application-version 1 ";
 
         public Form1()
@@ -25,50 +26,20 @@ namespace ota_package_gen_gui
 
         private void btn_keyfile_Click(object sender, EventArgs e)
         {
-            Process P = Process.Start(filepath, param_keyfile);
-            P.WaitForExit();
-            int result = P.ExitCode;
-            if(result == 0)
-            {
-                String dest = "dist\\temp.pem";
-                
-                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-
-                saveFileDialog1.Filter = "pem files (*.pem)|*.pem|All files (*.*)|*.*";
-                saveFileDialog1.FilterIndex = 1;
-                saveFileDialog1.RestoreDirectory = true;
-
-                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-                {
-                    dest = saveFileDialog1.FileName;
-                    if (File.Exists(dest))
-                    {
-                        File.Delete(dest);
-                    }
-                    File.Move("temp.pem", dest);
-                    keyfile_url.Text = dest;
-                    MessageBox.Show("Keyfile has been generated successfully", "Keyfile Generation", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-
-            } else
-            {
-                MessageBox.Show("Keyfile generation aborted", "Keyfile Generation", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
         }
 
         private void btn_package_Click(object sender, EventArgs e)
         {
-            String keyfile = keyfile_url.Text;
             String firmware = firmware_url.Text;
 
-            if(keyfile == "" || firmware == "")
+            if(firmware == "")
             {
-                MessageBox.Show("Please specify keyfile and firmware", "Package Generation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Please specify firmware", "Package Generation", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
             String zipfile = "temp.zip";
-            String param = param_package + "--application \"" + firmware + "\" --key-file \"" + keyfile + "\" " + zipfile;
+            String param = param_package + "--application \"" + firmware + "\" --key-file dist\\temp.pem " + zipfile;
 
             Process P = Process.Start(filepath, param);
             P.WaitForExit();
@@ -82,6 +53,7 @@ namespace ota_package_gen_gui
                 saveFileDialog1.Filter = "zip files (*.zip)|*.zip|All files (*.*)|*.*";
                 saveFileDialog1.FilterIndex = 1;
                 saveFileDialog1.RestoreDirectory = true;
+                saveFileDialog1.FileName = firmware_filename.Substring(0, firmware_filename.Length - 4);
 
                 if (saveFileDialog1.ShowDialog() == DialogResult.OK)
                 {
@@ -95,23 +67,13 @@ namespace ota_package_gen_gui
                 }
             } else
             {
-                MessageBox.Show("Error occured while generating DFU package. Please make sure keyfile or firmware is correct.", "Package Generation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Error occured while generating DFU package. Please make sure firmware is correct.", "Package Generation", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void btn_keyfile_browse_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
-            openFileDialog1.InitialDirectory = "c:\\";
-            openFileDialog1.Filter = "pem files (*.pem)|*.pem|All files (*.*)|*.*";
-            openFileDialog1.FilterIndex = 1;
-            openFileDialog1.RestoreDirectory = true;
-
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                keyfile_url.Text = openFileDialog1.FileName;
-            }
         }
 
         private void btn_firmware_browse_Click(object sender, EventArgs e)
@@ -127,6 +89,7 @@ namespace ota_package_gen_gui
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 firmware_url.Text = openFileDialog1.FileName;
+                firmware_filename = openFileDialog1.SafeFileName;
             }
         }
     }
